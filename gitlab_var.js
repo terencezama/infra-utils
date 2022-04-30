93e200114367c14c429966aacf81b304e1f20872
Terence Zama <terence.zama@gmail.com>
2022-05-01 00:50:06 +0400

    first commit

---
+++ gitlab_var.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const private_token = process.env.GITLAB_TOKEN;
+const project_id = "14175408";
+
+const axios = require('axios');
+const config = {
+    headers: {
+        "PRIVATE-TOKEN": private_token
+    }
+}
+
+axios.get(`https://gitlab.com/api/v4/projects/${project_id}/variables`, config).then( res => {
+    let map = {};
+    res.data.forEach(gitVar => {
+        const { environment_scope, key, value } = gitVar;
+        if (!(environment_scope in map)) {
+            map[environment_scope] = [];
+        }
+
+        map[environment_scope].push({
+            key,
+            value
+        })
+    });
+    
+    for (const mkey in map) {
+        console.log(`\n########################`);
+        console.log(`[${mkey}]`)
+        map[mkey].forEach( elem => {
+            const { key, value } = elem;
+            console.log(`${key}=${value}`)
+        });
+        console.log(`########################\n`);
+    }
+});
