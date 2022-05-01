fce21ce290aeb3de1cb7ffd7566ad7a599a2258b
Terence Zama <terence.zama@gmail.com>
2022-05-01 06:57:27 +0400

    improved variable setter

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # infra-utils
+
+### aws_var
+```
+ aws_var <app_name> <env_name> <path>
+
+options:
+    - -a appends
+````
--- aws_var.js
+++ aws_var.js
@@ -1,11 +1,25 @@
-// aws elasticbeanstalk describe-configuration-settings --environment-name goldilock-sbox --profile circle
+
 require('dotenv').config();
-var AWS = require('aws-sdk');
+const argv = require('minimist')(process.argv.slice(2));
+const AWS = require('aws-sdk');
+const { exit } = require('process');
+const fse = require('fs-extra');
 
 AWS.config.region = 'eu-west-1';
+
+console.log(argv);
+if (argv._.length !== 3) {
+    exit();
+}
+
+
+const appName = argv._[0];
+const envName = argv._[1];
+const output = argv._[2];
+
 const res = new  AWS.ElasticBeanstalk().describeConfigurationSettings({
-    ApplicationName: 'goldilocks',
-    EnvironmentName: 'goldilocks-sbox'
+    ApplicationName: appName,
+    EnvironmentName: envName
 }).promise();
 
 
@@ -13,7 +27,13 @@ res.then((value) => {
 
     const { OptionSettings } = value.ConfigurationSettings[0];    
     const option = OptionSettings.filter(opt => opt.OptionName === 'EnvironmentVariables')[0];
-    const values = option.Value.split(',');
-    values.forEach( val => console.log(val));
+    const values = option.Value.replace(/,/g, '\n');
+    if ('a' in argv) {
+        fse.appendFileSync(output, values);
+    } else {
+        fse.outputFileSync(output, values);
+    }
+    
+    
 
 })
